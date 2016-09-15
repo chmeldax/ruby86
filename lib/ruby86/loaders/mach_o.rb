792c51f250275756b2857efc279068af93e33601
Jan Chmelicek <jan.chmelicek@simplybusiness.co.uk>
2016-09-15 09:31:09 +0100

    Working version of header parser

--- lib/ruby86/loaders/mach_o.rb
+++ lib/ruby86/loaders/mach_o.rb
@@ -1,46 +1,80 @@
 module Ruby86
   module Loaders
     class MachO
-      Header = Struct.new(:cpu_type, :cpu_subtype, :filetype, :ncmds, :sizeofcmds, :flags, :reserved)
+      attr_reader :file, :segments
 
-      def initialize(file_path)
-        @file = File.open(file_path)
+      Header = Struct.new(:cpu_type, :cpu_subtype, :filetype, :ncmds, :sizeofcmds, :flags, :reserved)
+      Segment = Struct.new(:name, :vm_address, :vm_size, :file_offset, :file_size)
+
+      def initialize(file_path, memory)
+        @file = File.open(file_path, 'rb')
+        @memory = memory
+        @segments = []
       end
 
       def load!
         check_magic_number
         header = load_header
-        sizes = 0
         header.ncmds.times do
-          command = read_uint32(1*4).first
-          size = read_uint32(1*4).first
-          sizes += size
-          handle_command(command, read_uint32(size - 8))
+          command = read_uint32
+          size = read_uint32
+          handle_command(command, size)
         end
+        load_segments!
       end
 
       private
 
       def check_magic_number
-        magic_number = @file.read(4).unpack('N')
-        raise 'Unsupported architecture' unless magic_number.first == 0xcffaedfe # x64, big_endian
+        magic_number = read_uint32
+        raise 'Unsupported architecture' unless magic_number == 0xfeedfacf # x64, little_endian
       end
 
       def load_header
-        header_raw = read_uint32(7 * 4)
+        header_raw = file.read(7 * 4).unpack('L7')
         Header.new(*header_raw)
       end
 
-      def read_uint32(count)
-        @file.read(count).unpack("V#{count/4}")
+      def read_uint32
+        file.read(4).unpack('L').first
       end
 
-      def handle_command(command, raw_command)
+      def read_uint64
+        file.read(8).unpack('Q').first
+      end
+
+      def read_char
+        file.read(1).unpack('C').first.chr
+      end
+
+      def handle_command(command, size)
         case command
-        when 0x19 #LC_SEGMENT
-          puts 'LC_SEGMENT_64'
-        when 0x80000028 #LC_UNIXTHREAD
-          puts 'LC_MAIN'
+        when 0x19 # LC_SEGMENT_64
+          handle_segment(size)
+        when 0x80000028 # LC_MAIN
+          # FIXME
+        else
+          # noop
+        end
+      end
+
+      def handle_segment(size)
+        name = ''
+        16.times { name += read_char }
+        vm_address = read_uint64
+        vm_size = read_uint64
+        file_offset = read_uint64
+        file_size =  read_uint64
+        @file.read(size - (2*4 + 16 +  4*8))
+
+        segments << Segment.new(name, vm_address, vm_size, file_offset, file_size)
+      end
+
+      def load_segments!
+        segments.each do |segment|
+          segment.file_size.times do |i|
+            @memory[segment.vm_address + i] = @file.read(1)
+          end
         end
       end
     end
--- spec/ruby86/loaders/mach_o_spec.rb
+++ spec/ruby86/loaders/mach_o_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 describe Ruby86::Loaders::MachO do
   context 'correct file' do
-    subject { described_class.new(path) }
+    subject { described_class.new(path, memory) }
     let(:path) { File.expand_path File.dirname(__FILE__) + './../../fixtures/mach_o/print.out' }
-
+    let(:memory) { {} }
     describe '#load!' do
+      let(:segments_count) { 4 }
+      let(:memory_size) { 4096 + 4096 + 240 }
       it 'runs without error' do
         expect{subject.load!}.to_not raise_error
+      end
+
+      it 'load four segments' do
+        expect{subject.load!}.to change{subject.segments.count}.from(0).to(segments_count)
+      end
+
+      it 'fills memory with correct size' do
+        expect{subject.load!}.to change{memory.to_a.count}.from(0).to(memory_size)
       end
     end
   end
